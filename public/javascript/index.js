4a8e068372ac57e4e4ab17de87e1df236bb3f35c
TomMahe <mahettom@gmail.com>
2023-02-19 18:07:18 +0100

    need to finish

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,26 +1,62 @@
-  /**
-   * You might want to use this template to display each new characters
-   * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/template#examples
-   */
-  const characterTemplate = document.getElementById('template')
+const characterTemplate = document.getElementById('template')
 
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
 
-  });
+// CREATE A CUSTOM AXIOS INSTANCE FOR THE FUTUR API REQUEST
+const characterApi = axios.create({
+  baseURL: 'http://localhost:5005/api/characters/',
+})
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+// STORE URL IN VARIABLE FOR REUSE IT LATER
+const myUrl = 'http://localhost:5005/api/'
 
-  });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
 
-  });
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+// BEFORE THE EVENT LISTENER ———> CREATE THE FUNCTIONS WE WILL NEED LATER
 
-  });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+// function createCharacter(one) {
 
-  });
+
+//   const clone = characterTemplate.content.cloneNode(true)
+
+
+// }
+
+
+// LISTEN FOR THE FETCH-ALL BUTTON AND DISPLAY ALL CHARACTERS
+document.getElementById('fetch-all').addEventListener('click', async function (event) {
+  try {
+
+    const { data } = await axios.get(`${myUrl}characters`)
+
+    // RETURN A OBJECT CONTAINING THE VALUE OF EACH ONE
+    for (const character of data) {
+      console.log(character);
+
+      //const clone =
+
+      return character
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+document.getElementById('fetch-one').addEventListener('click', function (event) {
+
+});
+
+document.getElementById('delete-one').addEventListener('click', function (event) {
+
+});
+
+document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+
+});
+
+document.getElementById('new-character-form').addEventListener('submit', function (event) {
+
+});
 
--- routes/characters.route.js
+++ routes/characters.route.js
@@ -1,45 +1,83 @@
 const router = require('express').Router()
 const Character = require('../models/Character.model')
-/**
- * !All the routes here are prefixed with /api/characters
- */
 
-/**
- * ? This route should respond with all the characters
- */
-router.get('/', (req, res, next) => {
-	/**Your code goes here */
+
+// ALL ROUTES IN HERE ARE PREFIXED WITH ——> /api/characters
+
+
+// ROUTE FOR RENDERING ALL CHARACTERS   ———————————   RENDER ALL CHARACTER ———————————————————
+router.get('/', async (req, res, next) => {
+	try {
+		// LIST ALL THE CHARACTERS
+		// RETURN AN OBJECT WITH THE VALUE OF EACH CHARACTER
+		const allCharacters = await Character.find()
+		console.log(allCharacters)
+		res.status(200).json(allCharacters)
+
+	} catch (error) {
+		next(error);
+	}
 })
 
-/**
- * ? This route should create one character and respond with
- * ? the created character
- */
-router.post('/', (req, res, next) => {
-	/**Your code goes here */
+
+// LISTEN TO POST FOR CREATE ONE CHARACTER    ——————————————  CREATE ROUTE —————————————————————
+router.post('/', async (req, res, next) => {
+	try {
+		// GET INPUT FROM THE FORM & CREATE ONE WITH THE INFO
+		const oneToCreate = { ...req.body }
+		const createdOne = await Character.create(oneToCreate)
+		// CREATION STATUS CODE
+		// TAKE JSON INPUT TO GIVE BACK JS OBJECT WITH THE INFORMATIONS 
+		res.status(201).json(createdOne)
+	} catch (error) {
+		next(error)
+	}
 })
 
-/**
- * ? This route should respond with one character
- */
-router.get('/:id', (req, res, next) => {
-	/**Your code goes here */
+
+//————————————————————————————————————————————————————— GET ONE BY ID ROUTE ———————————————————
+router.get('/:id', async (req, res, next) => {
+	try {
+		const currentOne = await Character.findById(req.params.id)
+		res.json(currentOne)
+
+	} catch (error) {
+		next(error)
+	}
 })
 
-/**
- * ? This route should update a character and respond with
- * ? the updated character
- */
-router.patch('/:id', (req, res, next) => {
-	/**Your code goes here */
+//—————————————————————————————————————————————————————————— UPDATE ROUTE ———————————————————————
+router.patch('/:id', async (req, res, next) => {
+	const { id } = req.params
+	const oneToUpdate = { ...req.body }
+	try {
+		const characterUpdate = await Character.findByIdAndUpdate(
+			id,
+			oneToUpdate,
+			{ new: true }
+		)
+
+		res.json(characterToUpdate)
+	} catch (error) {
+		next(error)
+	}
 })
 
-/**
- * ? Should delete a character and respond with a success or
- * ? error message
- */
-router.delete('/:id', (req, res, next) => {
-	/**Your code goes here */
+
+//——————————————————————————————————————————————————————————— DELETE ROUTE ——————————————————————
+router.delete('/:id', async (req, res, next) => {
+	const { id } = req.params
+	const oneToDelete = { ...req.body }
+
+	try {
+
+		const deleteCharacter = await Character.findByIdAndDelete(oneToDelete)
+
+		res.json({ message: `The character ${id} has been successfully deleted` })
+
+	} catch (error) {
+		next(error)
+	}
 })
 
 module.exports = router
